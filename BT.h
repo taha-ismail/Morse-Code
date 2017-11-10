7bb96187ea7f1d20958eedf57b32daf6545f28e6
adropofilm <adropofilm@gmail.com>
2017-11-10 15:09:04 -0600

    Tree Structure Header
    This file contains the generic variables and functions needed for the tree.

---
+++ BT.h
@@ -0,0 +1,29 @@
+#include <string>
+#include <cctype>
+#include <iostream>
+#include <fstream>
+#ifndef BT_H
+#define BT_H
+
+
+using namespace std;
+class BT {
+private:
+	struct BTNode {
+		BTNode *left_node;
+		BTNode *right_node;
+		string code;
+		string the_message;
+	}; BTNode *root_node;
+
+public:
+	BT();
+	~BT();
+	string decode(string input, bool added);
+	string encode(string input, bool added);
+	string inputtedText();
+	void insert_code(char message, string code);
+	void insert_code(BTNode *&new_node, char message, string code, bool is_last);
+	bool leafNode(BTNode *node);
+};
+#endif

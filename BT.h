4f45e0cabfacc00ba6ae4ce37855c4e4f020b968
adropofilm <adropofilm@gmail.com>
2017-11-29 14:19:56 -0600

    refactor variables

--- BT.h
+++ BT.h
@@ -12,19 +12,19 @@ private:
 	struct BTNode {
 		BTNode *left_node;
 		BTNode *right_node;
-		string code;
-		string the_message;
-	}; BTNode *root_node;
+		string morse_code;
+		string node_to_add;
+	}; BTNode *tree_root_node;
 
 public:
 	BT();
 	~BT();
-	string decode(string input, bool added);
-	string encode(string input, bool added);
-	string inputtedText();
-	void insert_code(char message, string code);
-	void insert_code(BTNode *&new_node, char message, string code, bool is_last);
+	string decode(string user_input, bool added_node);
+	string encode(string user_input, bool added_node);
+	string text_entered();
+	void insert_code(char node_to_add, string morse_code);
+	void insert_code(BTNode *&new_node, char node_to_add, string morse_code, bool is_last);
 	bool leafNode(BTNode *node);
-	bool lessFour(string::size_type a);
+	bool less_than_four(string::size_type a);
 };
 #endif

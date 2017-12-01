1a4f3b2b88b0a7bd71f4aa5f2ccfa97cf48a57a0
taha-ismail <33557439+taha-ismail@users.noreply.github.com>
2017-12-01 14:17:24 -0600

    Comments

--- BinaryTree.h
+++ BinaryTree.h
@@ -16,19 +16,28 @@ private:
 	BinaryTreeNode *left_child;
 	BinaryTreeNode *right_child;
 }; BinaryTreeNode *node_root;
+
 public:
+	// REQUIRED:
 	 BinaryTree();
 	~BinaryTree();
 
 	// SETTERS:
-	void insert_code(char char_value, string morse_value); // Inserts only root node.
-	void insert_code(BinaryTreeNode *&new_node, char char_value, bool last_character); // Inserts all non-root nodes.
+	// Inserts only root node.
+	void insert_code(char char_value, string morse_value); 
+	// Inserts all non-root nodes.
+	void insert_code(BinaryTreeNode *&new_node, char char_value, bool last_character); 
 
 	// ENCODING AND DECODING FUNCTIONS:
+	// Decodes morse code into english.
 	string decode(string users_morse_string, bool added_node);
+	// Encodes english into morse code.
 	string encode(string users_morse_string, bool added_node);
+	// Used to retrieve user input
 	string get_input();
+	// Validates if node is leafe node
 	bool validate_leaf(BinaryTreeNode *node);
+	// Validates length of node, if <=3.
 	bool validate_length(unsigned int a);
 };
 #endif
